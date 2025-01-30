019ad2b5b5a68f081a69200499e55269e2fdf145
Mrrll <edgarmarrollo@gmail.com>
2025-01-30 15:16:20 +0100

    Eliminados archivos obsoletos de transformación y migración de datos, simplificando el proceso interactivo.

--- transformar.php
+++ transformar.php
@@ -1,4 +1,5 @@
 <?php
+require_once "funciones.php";
 // ==========================================
 // PROCESO INTERACTIVO
 // ==========================================
@@ -6,34 +7,23 @@ function transformarValores($base_origin, $tablas_origin, $pdo_origin)
 {
     while (true) {
         echo "\nProceso para transformar valores en la base de datos de origen ($base_origin):\n";
-        $transformar = obtenerEntradaValida("> ¿Estas seguro que quieres transformar los valores de algún campo en la base de datos de origen ($base_origin)? (si/no): ", ['si', 'no']);
+        $transformar = obtenerEntradaValida("> ¿Estas seguro que quieres transformar los valores de algún campo en la base de datos de origen ($base_origin)? (si/no): ", ['si', 'no']);        
+
+        
         if ($transformar !== 'si') {
-            $cambiar_valor = obtenerEntradaValida("> ¿Quieres cambiar el valor de algún campo en la base de datos de origen ($base_origin)? (si/no): ", ['si', 'no']);
-            if ($cambiar_valor !== 'si') {
-                break;
-            }
+            echo "\nProceso de transformación finalizado.\n";
+            break;
         }
 
         $tabla_origin = seleccionarTabla($base_origin, $tablas_origin, $pdo_origin);
         $campo_origin = seleccionarCampo($tabla_origin, $tablas_origin);
-
-        if ($transformar === 'si') {
-            transformarCampo($base_origin, $tabla_origin, $campo_origin, $pdo_origin);
-        } else {
-            actualizarCampo($base_origin, $tabla_origin, $campo_origin, $pdo_origin);
-        }
+        
+        transformarCampo($base_origin, $tabla_origin, $campo_origin, $pdo_origin);
 
         $continuar = obtenerEntradaValida("> ¿Deseas realizar otra operación? (si/no): ", ['si', 'no']);
         if ($continuar !== 'si') {
-            echo "Proceso finalizado. ¡Hasta luego!\n";
+            echo "\nProceso de transformación finalizado.\n";
             break;
         }
     }
 }
-
-
-
-
-
-
-
